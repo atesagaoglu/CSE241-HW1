d9151bb2af1599229e9c68cd87ba2ea1c54b1e35
atesagaoglu <atesagaoglu.mertemre@gmail.com>
2022-10-20 12:51:18 +0300

    branch br1

--- HW1/tetromino.h
+++ HW1/tetromino.h
@@ -5,21 +5,7 @@
 #include "enums.h"
 
 class Tetromino {
-	public:
-		//constructors
-		Tetromino(); //no-parameter inits to o tetramino
-		Tetromino(TetrominoType type); //no-parameter
-
-		//member functions
-		bool rotate(RotationDirection dir);
-		bool print();
-		//bool canFit();
-		//will implement canFit later
-	private:
-		std::vector<std::vector<char>> grid; //multidimensional vector
-		
-
-
+	
 };
 
 #endif
